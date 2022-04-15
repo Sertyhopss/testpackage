91e85e9d71bdd914f73f0f0358cc236e2d2afae5
Ivan Maximchuk <56026295+Sertyhopss@users.noreply.github.com>
2022-04-15 15:57:48 +0300

    testing some things

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test_package",
+    version="0.0.1",
+    author="sertyhopss",
+    author_email="srt@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sertyhopss/testpackage",
+    project_urls={
+        "Bug Tracker": "https://github.com/Sertyhopss/testpackage/issues",
+    },
+    packages=['test_package'],
+    install_requires=['random', 'numpy'],
+)
---
+++ testpackage/test.py
@@ -0,0 +1,6 @@
+import random as rnd
+import numpy as np
+
+
+def tester():
+    print('WOW IT WORKS. YOU L{}SER'.format(np.int(rnd.randint(0, 10))))

c600fce03b9dc6ec3021af957a1f3c3536f08e32
Ivan Maximchuk <56026295+Sertyhopss@users.noreply.github.com>
2022-04-15 15:58:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="test_package",
+    name="testpackage",
     version="0.0.1",
     author="sertyhopss",
     author_email="srt@example.com",

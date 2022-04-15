7dff9285a3c3543ca941762529e1974ccaac6c6e
Ivan Maximchuk <56026295+Sertyhopss@users.noreply.github.com>
2022-04-15 16:06:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="testpackage",
-    version="0.0.1",
+    version="0.0.2",
     author="sertyhopss",
     author_email="srt@example.com",
     description="A small example package",
